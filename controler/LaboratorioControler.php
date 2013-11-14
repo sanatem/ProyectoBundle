8aaf221c31c370c3b8b45cbaa936484554a1bb9b
Esteban Sanchez <est-ban06@hotmail.com>
2013-11-14 00:19:09 -0300

    controlador

---
+++ controler/LaboratorioControler.php
@@ -0,0 +1,14 @@
+<?php
+
+//Conectamos a la base
+require_once("../model/modelo.php");
+class claseControler {
+
+	function nombrefuncion() {
+	
+	}
+
+}
+
+
+?>
---
+++ controler/indexControler.php
@@ -0,0 +1,9 @@
+<?php
+
+
+define('RAIZ_APLICACION', '/var/www/grupo_51/');
+define('RAIZ_SITIO', 'http://localhost.proy/controler/indexControler.php?');
+
+require_once(RAIZ_APLICACION."/controler/router.php");
+
+?>
