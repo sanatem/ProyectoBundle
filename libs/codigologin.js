bfbf7bbc41c03e881d5b26cedf1b4b9091946484
Esteban Sanchez <est-ban06@hotmail.com>
2013-11-14 00:32:11 -0300

    modeloss

---
+++ libs/codigologin.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("banner1").click(function(){
+    $(this).fadeOut();
+  });
+});
+function esconder(){
+	$("#banner1").hide();
+	$("#centrado").hide();
+}
+window.setTimeout(function(){	$("#banner1").fadeIn("slow"); $("#centrado").fadeIn("slow");},500);
--- model/modelo.php
+++ model/modelo.php
@@ -1,19 +1,90 @@
 <?php
-function obtener_usuarios(){
-	$db_host="127.0.0.1";
-	$db_user="user";
-	$db_pass="pass";
-	$db_base="base"; 
-	$link = mysqli_connect($db_host,$db_user,$db_pass,$db_base) or die("Error " . mysqli_error($link));
-	$resu=$link->query("select * from usuarios");
+require_once("conexionBase.php");
 
-	while ($dato = mysqli_fetch_array($resu))  {
-		$usuarios[]=$dato;
+function logearse($us, $con) {
+	$link = conectarBaseDatos();
+	if ($link != "error"){
+	 $query = $link->prepare("SELECT * FROM usuario where nombre= :Nombre And contrasena= :Contrasena");
+	 $query->execute(array('Nombre' => $us,
+							'Contrasena' => $con));
+	 $res = $query->fetch();
+	 
+	}else {
+		$res= "error";
+		
 	}
-	// Cierro la conexión
-	mysqli_close($link);
-	return $usuarios;
+	return $res;
+	
 }
+ function cargarUsuario($us, $con, $tipo) {
+	$link = conectarBaseDatos();
+	if ($link != "error"){
+		 $query = $link->prepare("INSERT INTO `usuario`(`nombre`, `contrasena`, `tipo_roll`) VALUES (:Nombre, :Contrasena, :Tipo_roll)");
+		 $res = $query->execute(array('Nombre' => $us,
+								'Contrasena' => $con,
+								'Tipo_roll' => $tipo)) ;
+
+	}else {
+		$res= "error";
+		
+	}
+	return $res;
+
+  }
+
+function obtenerTipos(){
+	$link = conectarBaseDatos();
+	if ($link != "error"){
+	 $query = $link->prepare("SELECT * FROM roll ");
+	 $query->execute();
+	 $res = $query->fetchAll();
+	 
+	}else {
+		$res= "error";
+		
+	}
+	return $res;
+
+}
+
+ function obtenerUsuario($us){
+	$link = conectarBaseDatos();
+	if ($link != "error"){
+	 	$query = $link->prepare("SELECT nombre FROM usuario where nombre= :Nombre");
+	 	$query->execute(array('Nombre' => $us)); 
+	 	$res=$query->fetch();
+	 	 
+	 }else {
+	 	$res= "error";
+	 }
+	 return $res;
+}
+
+ function obtenerUsuarios(){
+	$link = conectarBaseDatos();
+	if ($link != "error"){
+	 	$query = $link->prepare("SELECT `nombre`,`tipo_roll`,`id_us` FROM usuario");
+	 	$query->execute();
+	 	$res=$query->fetchAll();
+	 	 
+	 }else {
+	 	$res= "error";
+	 }
+	 return $res;
+}
+function eliminarUsuario($id_us) {
+	$link = conectarBaseDatos();
+	if ($link != "error"){
+		 $query = $link->prepare("DELETE FROM `usuario` WHERE `id_us`= :Id_us");
+		 $res = $query->execute(array('Id_us' => $id_us));
+	}else {
+		$res= "error";
+		
+	}
+	return $res;
+
+ }
+
 ?> 
 
 
