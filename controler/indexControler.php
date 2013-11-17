8dd58144dcc4cb109d09d2ad55dd0bc8e950ed20
Esteban Sanchez <est-ban06@hotmail.com>
2013-11-17 02:23:34 -0300

    router

--- controler/indexControler.php
+++ controler/indexControler.php
@@ -1,9 +1,7 @@
 <?php
-
-define('RAIZ_APLICACION', 'https://git.proyecto2013.linti.unlp.edu.ar/');
-define('RAIZ_SITIO', 'https://grupo_51.proyecto2013.linti.unlp.edu.ar/controler/indexControler.php?');
+define('RAIZ_APLICACION', '/grupo_51/');
+define('RAIZ_SITIO', 'http://localhost/controler/indexControler.php?');
 
 require_once(RAIZ_APLICACION."/controler/router.php");
-
 
 ?>
