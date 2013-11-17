3cfdbaceba5bd53dc109479a805caa64fca56990
Esteban Sanchez <est-ban06@hotmail.com>
2013-11-17 02:35:51 -0300

    intento3

--- controler/indexControler.php
+++ controler/indexControler.php
@@ -1,5 +1,5 @@
 <?php
-define('RAIZ_APLICACION', 'localhost/');
+define('RAIZ_APLICACION', 'https://git.proyecto2013.linti.unlp.edu.ar/ayudantes/grupo_51/');
 define('RAIZ_SITIO', 'https://grupo_51.proyecto2013.linti.unlp.edu.ar/controler/indexControler.php?');
 
 require_once(RAIZ_APLICACION."/controler/router.php");
