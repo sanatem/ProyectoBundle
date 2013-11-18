e69a66c3249f1c300937062646368548614ad308
Vilches Diego <dvilches@info.unlp.edu.ar>
2013-11-18 10:01:29 -0300

    Modificado por diego

--- controler/indexControler.php
+++ controler/indexControler.php
@@ -2,5 +2,5 @@
 define('RAIZ_APLICACION', 'https://git.proyecto2013.linti.unlp.edu.ar/ayudantes/grupo_51/tree/master/');
 define('RAIZ_SITIO', 'https://grupo_51.proyecto2013.linti.unlp.edu.ar/controler/indexControler.php?');
 
-require_once(RAIZ_APLICACION."/controler/router.php");
+require_once("/controler/router.php");
 ?>
