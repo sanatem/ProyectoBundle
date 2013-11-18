38c0fddedf0b05ddc3fb1861ded6b9c911ae7e79
Esteban Sanchez <est-ban06@hotmail.com>
2013-11-18 07:38:59 -0300

    andara

--- controler/indexControler.php
+++ controler/indexControler.php
@@ -1,5 +1,5 @@
 <?php
-define('RAIZ_APLICACION', '../grupo51');
+define('RAIZ_APLICACION', '../grupo_51.proyecto2013.linti.unlp.edu.ar/');
 define('RAIZ_SITIO', 'https://grupo_51.proyecto2013.linti.unlp.edu.ar/controler/indexControler.php?');
 
 require_once(RAIZ_APLICACION."/controler/router.php");
