40ec1a9ccbe95147773a427d990940404c846bd3
Santiago Pravisani <santiagopravisani@gmail.com>
2014-04-13 23:53:59 -0300

    Borrando mas cosas

--- controler/indexControler.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('RAIZ_SITIO', 'https://grupo_51.proyecto2013.linti.unlp.edu.ar/controler/indexControler.php?');
-
-require_once("./router.php");
-?>
--- libs/codigoAdmin.js
+++
@@ -1,4 +0,0 @@
-function esconder(){
-	$(".laboratorix").hide();
-	window.setTimeout(function(){	$(".laboratorix").fadeIn("slow");},500);
-}
